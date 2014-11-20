b68af28f13808388e8ef11b06453949687e0f39a
Pablo Scasso <pablo@Pablos-MacBook-Pro.local>
2014-11-20 13:18:39 -0200

    Changed main path to be '/main' instead of '/'

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,54 +19,55 @@ angular
         'ui.router'
     ])
 
-    .run(function($rootScope, $log) {
-        $rootScope.$log = $log;
-    })
+    .run([
+        '$rootScope',
+        '$log',
+        function($rootScope, $log) {
+            $rootScope.$log = $log;
+        }])
 
-    .config(function($stateProvider, $routeProvider, $locationProvider) {
-
-        $routeProvider.otherwise('/');
-
-        $stateProvider
-            .state('main', {
-                url: '/',
-                templateUrl: 'views/main.html'
-            })
-            .state('options', {
-                url: '/options',
-                templateUrl: 'views/options.html'
-            })
-            .state('level', {
-                url: '/level',
-                templateUrl: 'views/level.html',
-                controller: 'LevelCtrl'
-            })
-            .state('questionsLength', {
-                url: '/questions-length',
-                templateUrl: 'views/questions-length.html',
-                controller: 'QuestionsLengthCtrl'
-            })
-            .state('question', {
-                url: '/question',
-                templateUrl: 'views/question.html',
-                controller: 'QuestionCtrl'
-            })
-            .state('completed', {
-                url: '/completed',
-                // url: '/completed/:correct',
-                // resolve: {
-                //   correct: ['$stateParams', function($stateParams) {
-                //       return $stateParams.correct;
-                //     }
-                //   ]
-                // },
-                templateUrl: 'views/completed.html',
-                controller: 'CompletedCtrl'
-            });
-
-            $routeProvider.otherwise('/');
-            
-            // html5Mode on, so it works without hash
-            $locationProvider.html5Mode(true);
-
-    });
+    .config([
+        '$stateProvider',
+        '$urlRouterProvider',
+        '$locationProvider',
+        function($stateProvider, $urlRouterProvider, $locationProvider) {
+        
+                $stateProvider
+                    .state('main', {
+                        url: '/main',
+                        templateUrl: 'views/main.html'
+                    })
+                    .state('options', {
+                        url: '/options',
+                        templateUrl: 'views/options.html'
+                    })
+                    .state('level', {
+                        url: '/level',
+                        templateUrl: 'views/level.html',
+                        controller: 'LevelCtrl'
+                    })
+                    .state('questionsLength', {
+                        url: '/questions-length',
+                        templateUrl: 'views/questions-length.html',
+                        controller: 'QuestionsLengthCtrl'
+                    })
+                    .state('question', {
+                        url: '/question',
+                        templateUrl: 'views/question.html',
+                        controller: 'QuestionCtrl'
+                    })
+                    .state('completed', {
+                        url: '/completed',
+                        // url: '/completed/:correct',
+                        // resolve: {
+                        //   correct: ['$stateParams', function($stateParams) {
+                        //       return $stateParams.correct;
+                        //     }
+                        //   ]
+                        // },
+                        templateUrl: 'views/completed.html',
+                        controller: 'CompletedCtrl'
+                    });
+        
+                    $urlRouterProvider.otherwise('/main');
+            }]);
